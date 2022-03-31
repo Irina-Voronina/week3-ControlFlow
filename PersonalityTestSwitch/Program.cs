4dad1ae4e2a56b302732a5a2e46ee9d1742b92f5
Irina Voronina <irinaworonina@gmail.com>
2022-04-01 00:54:52 +0300

    add my third c# project

---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Personality test
+            //приложение просит пользователя ввести его любимый цвет
+            //на основе введеннного значения, консоль показывает пользователю некоторую обратную связь
+            //red - you are so romantic
+            //blue - you are a workaholic
+            //green - you care about the environment
+            //любое другое значение, то консоль выведет предложение "you are a {userInput} unicorn"
+
+            Console.WriteLine("Please enter your favorite color:");
+            string userInput = Console.ReadLine().ToLower(); //RED/Red/rED/rEd/reD --> red
+
+            switch(userInput)
+            {
+                case "red":
+                    Console.WriteLine("You are so romantic.");
+                    break;
+                case "blue":
+                    Console.WriteLine("You are a workaholic.");
+                    break;
+                case "green":
+                    Console.WriteLine("You care about the environment.");
+                    break;
+                default:
+                    Console.WriteLine($"You are a {userInput} unicorn.");
+                    break;
+            }
+
+            Console.WriteLine("Have a nice day.");
+
+        }
+    }
+}
